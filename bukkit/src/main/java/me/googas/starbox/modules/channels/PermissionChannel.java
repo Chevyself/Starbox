e2217745fbc3e3bd0d40c82c2c9fd439ecfee489
xChevy <chevyself@gmail.com>
2021-08-24 15:30:09 -0500

    Add PermissionChannel

---
+++ bukkit/src/main/java/me/googas/starbox/modules/channels/PermissionChannel.java
@@ -0,0 +1,54 @@
+package me.googas.starbox.modules.channels;
+
+import lombok.NonNull;
+import me.googas.starbox.modules.channels.Channel;
+import me.googas.starbox.modules.channels.ConsoleChannel;
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Represents a channel which may only send data to players with an specific permission, this does also
+ * include the console.
+ */
+public class PermissionChannel implements Channel {
+
+    @NonNull
+    private final String permission;
+
+    /**
+     * Create the channel.
+     *
+     * @param permission the permission that players in this channel may have
+     */
+    public PermissionChannel(@NonNull String permission) {
+        this.permission = permission;
+    }
+
+    @NonNull
+    private Stream<? extends Player> filter() {
+        return Bukkit.getOnlinePlayers().stream().filter(player -> player.hasPermission(this.permission));
+    }
+
+    @Override
+    public @NonNull PermissionChannel send(@NonNull BaseComponent... components) {
+        ConsoleChannel.getInstance().send(components);
+        this.filter().map(Channel::of).forEach(channel -> channel.send(components));
+        return this;
+    }
+
+    @Override
+    public @NonNull PermissionChannel send(@NonNull String text) {
+        this.filter().map(Channel::of).forEach(channel -> channel.send(text));
+        return this;
+    }
+
+    @Override
+    public Optional<Locale> getLocale() {
+        return Optional.empty();
+    }
+}
